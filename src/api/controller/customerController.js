5f0b281bdb1ab48ea1292bb826da40b481d6379d
arijitdeb1502 <arijitdeb.git@gmail.com>
2020-04-18 07:35:17 +0800

    Finishing off the customerController

--- src/api/controller/customerController.js
+++ src/api/controller/customerController.js
@@ -1,9 +1,25 @@
 const responses = require('../../constants/response');
+const customerService = require('../../service/customerService');
 
 const signup = (req,res) =>{
     
     let response={ ...responses.responseDetails.defaultResponse };
+    
+    try{
 
+        const responseFromService = await customerService.signup(req.body);
+        response.status=responses.responseDetails.customerSignupSuccess.status;
+        response.message=responses.responseDetails.customerSignupSuccess.message;
+        response.body=responseFromService;   
+
+    }catch(error){
+        console.log('Something went wrong: customerController: signup', error);
+        response.message = error.message;
+    }
     
-    
+    return res.status(response.status).send(response);
+}
+
+module.exports = {
+    signup: signup
 }
