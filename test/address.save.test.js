d72b5f3bfd5483c23cadd39a125aa862fc1444dd
arijitdeb1502 <arijitdeb.git@gmail.com>
2020-05-30 08:53:36 +0800

    Start writing tests for Address Controller POST Address endpoint

---
+++ test/address.save.test.js
@@ -0,0 +1,79 @@
+const request = require('supertest');
+const jwt = require('jsonwebtoken');
+
+const app = require('../src/app');
+
+const Customer = require('../src/db/model/customer');
+const Address = require('../src/db/model/address');
+
+const {  addressSuccessAllFlds,
+         addressSuccessAllFlds2 
+      } = require('./fixtures/addressData');
+const {  customerSuccessAllFlds } = require('./fixtures/customerData');
+
+const base64EncodedCredentials="NjU5ODU4MTExMTphQmhpVGl0aGkxQDM=";
+const improperAccesToken="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZWFkZTNmN2M3MjM4MDFlMzA0NWU2MTYiLCJpYXQiOjE1ODg0NTQ0NzUsImV4cCI6MTU4ODQ2MTY3NX0.DTS1cfkcG3fDj8BvWvGlt1YBCxg6fRgHYcrRNJt0ER0";
+
+
+beforeEach(async () => {
+    await Customer.deleteMany();
+    await Address.deleteMany();
+})
+
+test('Testing the post address endpoint,SUCCESS Scenario1',async()=>{
+
+    //First signup the customer
+    const responseSignup=await request(app).post('/api/customer/signup').send({
+                ...customerSuccessAllFlds
+            }).expect(201);
+
+    const responseLogin=await request(app)
+                           .post('/api/customer/login')
+                           .set('authorization',base64EncodedCredentials)
+                           .send().expect(200);
+
+
+    const responseAddressSave1=await request(app)
+                           .post('/api/address')
+                           .set('authorization',responseLogin.header["access-token"])
+                           .send({
+                                    ...addressSuccessAllFlds
+                                }).expect(201);
+
+    expect(responseAddressSave1.body.id).not.toBe(null);
+    expect(responseAddressSave1.body.status).toBe("ADDRESS SUCCESSFULLY SAVED");
+
+    const responseAddressSave2=await request(app)
+                           .post('/api/address')
+                           .set('authorization',responseLogin.header["access-token"])
+                           .send({
+                                    ...addressSuccessAllFlds2
+                                }).expect(201);
+
+    expect(responseAddressSave2.body.id).not.toBe(null);
+    expect(responseAddressSave2.body.status).toBe("ADDRESS SUCCESSFULLY SAVED");
+    
+})
+
+
+test('Testing the post address endpoint,FAILURE Scenario1',async()=>{
+
+    //First signup the customer
+    const responseSignup=await request(app).post('/api/customer/signup').send({
+                ...customerSuccessAllFlds
+            }).expect(201);
+
+
+    const responseAddressSave1=await request(app)
+                           .post('/api/address')
+                           .set('authorization',improperAccesToken)
+                           .send({
+                                    ...addressSuccessAllFlds
+                                }).expect(401);
+
+    console.log(responseAddressSave1.body);
+
+    expect(responseAddressSave1.body.error).toBe('Please authenticate.');
+
+    
+})
---
+++ test/fixtures/addressData.js
@@ -0,0 +1,21 @@
+const addressSuccessAllFlds = {
+    flat_building_name : "18/P Jannagar Road",
+    locality : "Mahesh",
+    city : "Serampore",
+    pincode : "712202",
+    state_uuid : "009ae262-a234-11e8-b475-720006ceb890"
+}
+
+const addressSuccessAllFlds2 = {
+    flat_building_name : "201 Kalaberia Road",
+    locality : "Rajarhat",
+    city : "NewTown",
+    pincode : "700120",
+    state_uuid : "009ae262-a234-11e8-b475-720006ceb890"
+}
+
+
+module.exports= {
+    addressSuccessAllFlds,
+    addressSuccessAllFlds2
+}
