8cbd3b903970f8a97ae6cf1dc641b6236f61ac2d
arijitdeb1502 <arijitdeb.git@gmail.com>
2020-05-31 08:25:36 +0800

    Added test for get All states end point

---
+++ test/state.getall.test.js
@@ -0,0 +1,31 @@
+const request = require('supertest');
+const jwt = require('jsonwebtoken');
+
+const app = require('../src/app');
+
+const State = require('../src/db/model/state');
+
+const {  
+        expectedStates
+      } = require('./fixtures/statesData');
+
+test('Testing the getall state endpoint,SUCCESS Scenario1',async()=>{
+
+    const response=await request(app)
+                .get('/api/states')
+                .expect(200);
+
+    const actualStates={};
+    response.body.map((state)=>{
+        actualStates[state.state_uuid]=state.state_name;
+    })
+
+    const expectedData={}
+    for (state of expectedStates){
+        expectedData[state.state_uuid]=state.state_name
+    }
+
+    expect(response.body.length).toBe(36);
+    expect(expectedData).toEqual(actualStates);
+    
+})
