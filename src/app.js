908c6280fa36e78b897c1000653054d8ca2eaf3a
Arijit Deb <arijitdeb.git@gmail.com>
2020-09-20 17:55:58 +0800

    updated configurations so that non-docker version is being run instead of dockerized service

--- src/app.js
+++ src/app.js
@@ -4,9 +4,9 @@ const swaggerJSDoc = require('swagger-jsdoc');
 const swaggerUi = require('swagger-ui-express');
 const YAML = require('yamljs');
 
-// Swagger definition
+// Swagger definition config
 const swaggerDefinition = {
-    info: {
+    info:  {
       "title": 'REST API for Food Ordering app API', // Title of the documentation
       "version": '1.0.0', // Version of the app
       "description": 'This is the REST API for Food Ordering app API', // short description of the app
