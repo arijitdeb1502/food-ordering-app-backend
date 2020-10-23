2be17d4ba6033cc4180c12ff83ec5a94c61c8b6e
Arijit Deb <arijitdeb.git@gmail.com>
2020-10-23 19:37:32 +0800

    Updating package.json to include prod dependencies

--- src/server.js
+++ src/server.js
@@ -3,6 +3,8 @@ const port = process.env.PORT;
 
 //docker run command : docker run -p 3000:3001 arijithere/food-ordering-app-backend
 //docker compose run command : docker-compose up
+// https://adeb-test-food-order-backend.herokuapp.com/ | https://git.heroku.com/adeb-test-food-order-backend.git
+
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
